4592036dc17d836cacb5b8c1b9f0920bd1a332c2
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-14 14:10:25 +0530

    card init updates

---
+++ components/app-card/component.js
@@ -0,0 +1,15 @@
+class AppCard {
+    onCreate() {
+        let self = this;
+        self.state = {
+            imageData: null
+        }
+    }
+    onInput() {
+        let self = this;
+    }
+    onMount() {
+        let self = this;
+    }
+}
+module.exports = AppCard;
