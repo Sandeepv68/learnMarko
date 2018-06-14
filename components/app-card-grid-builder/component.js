6bc2c6cf0dc0216ecac3d6f5c7a8633c975c700a
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-14 15:57:39 +0530

    components app-card and app-card-builder updates

--- components/app-card-grid-builder/component.js
+++ components/app-card-grid-builder/component.js
@@ -1,29 +1,28 @@
 const Unsplash = require('../../services/unsplash');
 const unsplashConfig = require('../../config/unsplash');
 
-let UnsplashApi = new Unsplash(unsplashConfig);
-
 class AppCardBuilder {
     onCreate() {
         let self = this;
         self.state = {
             imageData: null
         };
-        console.log(self.state);
     }
     onInput() {
         let self = this;
-        console.log(self.state);
     }
     onMount() {
         let self = this;
-        // UnsplashApi.listPhotos(1, 10, 'latest')
-        // .then(function (result) {
-        //     console.log(result);
-        // }).catch(function (e) {
-        //     console.log(e);
-        // });
-        console.log(self.state);
+        let UnsplashApi = new Unsplash(unsplashConfig);
+        let page = 1;
+        let perPage = 6;
+        let orderBy = 'latest';
+        UnsplashApi.listPhotos(page, perPage, orderBy)
+            .then(function (result) {
+                self.state.imageData = result.data;;
+            }).catch(function (e) {
+                console.log(e);
+            });
     }
 }
 
--- components/app-card/component.js
+++ components/app-card/component.js
@@ -2,14 +2,21 @@ class AppCard {
     onCreate() {
         let self = this;
         self.state = {
+            input: null,
             imageData: null
         }
     }
-    onInput() {
+    onInput(input) {
+        if(!input || !input.data){
+            throw new Error('Error: Cannot render component -> app-card without input.');
+        }
+        input = input || {};
         let self = this;
+        self.state.input = input.data;
     }
     onMount() {
         let self = this;
+        self.state.imageData = self.state.input;
     }
 }
 module.exports = AppCard;
