3a939efc9923435fb1a8e5429f745408c233622e
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-15 13:09:47 +0530

    updates

---
+++ components/app-search/component.js
@@ -0,0 +1,20 @@
+class AppSearch {
+    getKeyword() {
+        let self = this;
+        let inputField = self.getEl('search_field');
+        let searchButton = self.getEl('search_button');
+        if (inputField.value.length >= 3) {
+            searchButton.classList.remove('hide');
+        } else {
+            searchButton.classList.add('hide');
+        }
+    }
+
+    emitKeyword() {
+        let self = this;
+        let inputField = self.getEl('search_field');
+        self.emit('keywordEntered', inputField.value);
+    }
+}
+
+module.exports = AppSearch;
