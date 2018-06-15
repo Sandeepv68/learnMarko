4291bfe69367b0a9ff2adaad11fe51762a5de150
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-15 12:36:29 +0530

    lazyloading updates

--- components/app-card-grid-builder/component.js
+++ components/app-card-grid-builder/component.js
@@ -5,8 +5,7 @@ class AppCardBuilder {
     onCreate() {
         let self = this;
         self.state = {
-            imageData: null,
-            imageDataAppend: null
+            imageData: null
         };
     }
     onInput() {
@@ -25,25 +24,40 @@ class AppCardBuilder {
                 console.log(e);
             });
         window.addEventListener('scroll', function () {
+            let parent = self.getEl('parent');
+            let imageAppend;
             let d = document.documentElement;
             let offset = d.scrollTop + window.innerHeight;
             let height = d.offsetHeight;
             if (offset === height) {
-                console.log('hit');
                 page = page + 1;
                 UnsplashApi.listPhotos(page, perPage, orderBy)
                     .then(function (result) {
-                        self.state.imageDataAppend = result.data;
+                        imageAppend = result.data;
+                        imageAppend.forEach(element => {
+                            $(parent).append(`<div class="col s12 m4 l4">
+                            <div class="card hoverable">
+                              <div class="card-image">
+                                <img src="${element.urls.small}" alt="photo"/>
+                              </div>
+                              <div class="card-content">
+                                <div class="chip animate-flicker">
+                                  <img src="${element.user.profile_image.small}" alt="photographer"/>
+                                  ${element.user.name}
+                                </div>
+                                <a class="grey lighten-3 waves-effect btn-flat right">
+                                  <i class="material-icons grey-text left">favorite</i>
+                                  ${element.likes}
+                                </a>
+                              </div>
+                            </div>
+                          </div>`);
+                        });
                     }).catch(function (e) {
                         console.log(e);
                     });
-
             }
         });
-    }
-    onUpdate(){
-        let self = this;
-        console.log(self.state);
     }
 }
 
